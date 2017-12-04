beaab3f650bd7aca524a287066855f55115ba192
jcools <jcools@seongsanlab.com>
2017-12-04 19:39:18 +0900

    init

---
+++ src/main/index.js
@@ -0,0 +1,13 @@
+import { app, screen, BrowserWindow } from "electron";
+import trimDesktop from "./trimDesktop";
+
+import createCaptureWindow from "./createCaptureWindow";
+
+let captureWindow;
+
+app.on("ready", ()=> {
+    // trimDesktop().then(({sourceDisplay, trimmedBounds}) => {
+    //     console.log(sourceDisplay,trimmedBounds);
+    // })    
+    captureWindow = createCaptureWindow();
+});
